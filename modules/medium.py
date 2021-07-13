37a154f4b0b4e82e184d7518f95f227e92b6d56b
Alize <alize_in_cryptoland@protonmail.com>
2021-07-13 19:32:21 +0200

    Add caching to medium

--- modules/medium.py
+++ modules/medium.py
@@ -2,9 +2,25 @@ import re
 from bs4 import BeautifulSoup
 import requests
 import json
+from pathlib import Path
+import os
+
+def getFilepath(id):
+  return os.getcwd() + "/out/medium/" + str(id) + ".json"
 
 def get_text(el):
   return el.get_text()
+
+def loadJson(name):
+  f = open(name)
+  if bool(f):
+    return json.load(f)
+
+
+def writeFile(name, content):
+  f = open(name, 'w')
+  f.write(content)
+  f.close()
 
 def scrape_medium(url):
   headers = {'User-Agent': 'Mozilla/5.0', 'Content-Language': 'en-US'}
@@ -12,7 +28,14 @@ def scrape_medium(url):
 
   soup = BeautifulSoup(page.content, 'html.parser')
 
+  name = getFilepath(id)
+
+  if Path(name).exists():
+    return loadJson(name)
+
   text = list(map(get_text, soup.findAll('p', id=re.compile(r'[^\s]{4}'))))
+
+  writeFile(name, text)
 
   return json.dumps(text)
 
